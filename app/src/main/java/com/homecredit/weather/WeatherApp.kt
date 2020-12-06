aec77709130fd22c5bd52fd7738ec4828b7c42e9
Russel Bulanon <bulanonrussel4@gmail.com>
2020-12-06 10:31:33 +0800

    Added application class and main activity

---
+++ app/src/main/java/com/homecredit/weather/WeatherApp.kt
@@ -0,0 +1,10 @@
+package com.homecredit.weather
+
+import android.app.Application
+
+class WeatherApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/com/homecredit/weather/ui/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.homecredit.weather.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.homecredit.weather.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+}
